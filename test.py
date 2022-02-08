95ba2adb8c8e90dac1bed1e9c4eda5eb16518049
Anastasiiall <a.p.chentsova@student.khai.edu>
2022-02-08 19:47:23 +0300

    Resolved conflict

--- test.py
+++ test.py
@@ -3,4 +3,8 @@ print("Python education")
 =======
 
 # new string
+<<<<<<< HEAD
 >>>>>>> 1c56e6b (test.py added to feature_branch with change on string#2)
+=======
+print("DICT")
+>>>>>>> 04d5747 (test.py added to feature_branch with change on string#3)
